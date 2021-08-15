5dea2ab9e013357a15326ef06df663dfec3e8fdc
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-14 20:33:45 -0400

    finished indexedb for offline function

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,13 +5,59 @@ const request = indexedDB.open('finance_tracker', 1);
 
 // event will emit if the database version changes
 request.onupgradeneeded = function(event) {
-    const db = event.target.result;
+    db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
-    const db = event.target.result;
+    db = event.target.result;
     if (navigator.onLine) {
-        sendTransaction();
+        uploadTransaction();
     }
 };
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    console.log(db);
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    transactionObjectStore.add(record);
+};
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                transactionObjectStore.clear();
+            })
+            .catch(err => console.log(err));
+        }
+    };
+};
+
+window.addEventListener('online', uploadTransaction);
